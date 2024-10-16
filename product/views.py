4b78be299c20fa40a5e548fc2dcb3815925e4051
jeonghun.son <ably@ABLY-MB-0510.local>
2024-10-16 12:05:40 +0900

    fix: 상품은 누구나 접근 가능

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.db.models import QuerySet
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
-from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from product.models import Product
@@ -11,6 +11,7 @@ from product.serializers import ProductSerializer
 # Create your views here.
 class ProductViewSet(viewsets.GenericViewSet):
     serializer_class = ProductSerializer
+    permission_classes = [AllowAny]  # 모든 요청 허용
 
     def get_queryset(self):
         queryset = Product.objects.all()
