535d8d9c6c0cca28f16273f1c882a0f7681cc494
jeonghun.son <ably@ABLY-MB-0510.local>
2024-10-16 09:33:30 +0900

    feat: 간단한 로그인 구현

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,20 @@
+import bcrypt
+
 from rest_framework import serializers
 from .models import User
+
+def encrypt_password(raw_password) -> str:
+    """비밀번호 암호화"""
+
+    byte_password: bytes = raw_password.encode("utf-8")
+    hashed_password: bytes = bcrypt.hashpw(byte_password, bcrypt.gensalt())
+
+    return hashed_password.decode("utf-8")
+
+def check_password(password: str, compare_password) -> bool:
+    """비밀번호 값 체크"""
+
+    return bcrypt.checkpw(password.encode("utf-8"), compare_password.encode("utf-8"))
 
 class UserSerializer(serializers.Serializer):
 
@@ -8,9 +23,13 @@ class UserSerializer(serializers.Serializer):
     name = serializers.CharField()
 
     def create(self, validated_data):
+
+        encrypted_password = encrypt_password(validated_data["password"])
+
         user = User.objects.create(
             loginId=validated_data['loginId'],
-            password=validated_data['password'],
+            password=encrypted_password,
             name=validated_data['name'],
         )
+
         return user
--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from user.models import User
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, check_password
 
 
 # Create your views here.
@@ -32,12 +32,6 @@ class UserViewSet(viewsets.GenericViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        # instance = User.objects.create(
-        #     loginId=request.data['loginId'],
-        #     password=request.data['password'],
-        #     name=request.data['name'],
-        # )
-
         instance = serializer.save()
 
         return Response({"id": instance.id}, status=status.HTTP_201_CREATED)
@@ -57,3 +51,25 @@ class UserViewSet(viewsets.GenericViewSet):
 
         serializer = self.get_serializer(user)
         return Response(serializer.data)
+
+    @action(methods=['POST'], detail=False, url_path="login")
+    def login(self, request) -> Response:
+        loginId: str | None = request.data.get("loginId")
+        password: str | None = request.data.get("password")
+
+        user: User | None = User.objects.filter(loginId=loginId).first()
+
+        if not user:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        if not check_password(password, user.password):
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        access_token = "Todo. token"
+
+        return Response(
+            {
+                "name": user.name,
+                "access_token": access_token
+            },
+            status.HTTP_200_OK,
+        )
