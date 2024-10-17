939b0b28d4b0b5909d3287838212a3d3c0e3753b
jeonghun.son <ably@ABLY-MB-0510.local>
2024-10-17 09:02:40 +0900

    feat: 도커파일 작성 & 도커컴포즈 파일 작성

--- server/settings.py
+++ server/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,17 +122,22 @@ WSGI_APPLICATION = "server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "pastel",
-        "USER": "pastel",
-        "PASSWORD": "1q2w3e4r",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "pastel",
+#         "USER": "pastel",
+#         "PASSWORD": "1q2w3e4r",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
